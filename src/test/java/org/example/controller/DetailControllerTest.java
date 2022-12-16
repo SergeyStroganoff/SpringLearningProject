486ae467e69562250e315d46184f049af9dc1ce1
Aspire5_Stroganow <111sergey90@gmail.com>
2022-12-16 20:08:04 +0300

    DetailControllerTest.java added - in progress.

---
+++ src/test/java/org/example/controller/DetailControllerTest.java
@@ -0,0 +1,54 @@
+package org.example.controller;
+
+import org.example.configuration.ConfigurationSpring;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations={"src/main/webapp/WEB-INF/applicationContext.xml"})
+@WebAppConfiguration
+@ActiveProfiles("test")
+@Disabled //TODO
+class DetailControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        Assertions.assertNotNull(servletContext);
+        Assertions.assertTrue(servletContext instanceof MockServletContext);
+        Assertions.assertNotNull(webApplicationContext.getBean("DetailController"));
+    }
+
+    @Test
+    void getDetailFromUI() {
+    }
+
+    @Test
+    void showResult() {
+    }
+}
